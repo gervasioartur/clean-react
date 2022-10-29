237cd6bb4578dd7385b908e11c1f84fbb7b9d920
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-29 16:14:48 -0300

    test: esnure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 import Login from './login'
 
 describe('Login Component', () => {
-  it('Should not render spinner and error on start', () => {
+  it('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })

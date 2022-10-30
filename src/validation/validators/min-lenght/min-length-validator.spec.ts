f9b2b123bc7e2aec868b1a2c134865294afafc1b
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-30 15:01:48 -0300

    refactor: cheange MinLengthValidation to use faker

--- src/validation/validators/min-lenght/min-length-validator.spec.ts
+++ src/validation/validators/min-lenght/min-length-validator.spec.ts
@@ -1,16 +1,18 @@
 import { InvalidFieldLengthError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
 import { MinLengthValidation } from './min-length-validator'
 
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.random.word(), 'fieldName', 5)
 describe('MinLengthValidator', () => {
   it('should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 'fieldName', 5)
-    const error = sut.validate('123')
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldLengthError('fieldName', 5))
   })
 
   it('should return falsy if value is valid', () => {
-    const sut = new MinLengthValidation('field', 'fieldName', 5)
-    const error = sut.validate('12345')
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })

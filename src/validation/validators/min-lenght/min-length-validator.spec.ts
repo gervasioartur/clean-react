30f9fdebac50b123ed1b96a9afdf5cb6a4725d4a
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-30 14:50:55 -0300

    test: ensure MinLengthValidator return falsy if value is valid

--- src/validation/validators/min-lenght/min-length-validator.spec.ts
+++ src/validation/validators/min-lenght/min-length-validator.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthValidator', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldLengthError('fieldName', 5))
   })
+
+  it('should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 'fieldName', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
 })

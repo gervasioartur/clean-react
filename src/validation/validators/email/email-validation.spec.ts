854bca162bcf5710183804cc87693d5075d1ecb6
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-30 14:22:56 -0300

    refactor: isolate SUT creation

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,16 +1,19 @@
 import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols'
 import { faker } from '@faker-js/faker'
 import { EmailValidation } from './email-validation'
 
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError('email'))
   })
 
   it('should return falsy if email is valid', () => {
-    const sut = new EmailValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,6 @@ export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
   validate (value: string): Error {
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
+    return emailRegex.test(value) ? null : new InvalidFieldError('email')
   }
 }

1db32be2dbc4cd64af53d889c5417bf2c440ca58
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-30 14:58:06 -0300

    feat: ensure MinLengthValidation not include spaces

--- src/validation/validators/min-lenght/min-length-validator.ts
+++ src/validation/validators/min-lenght/min-length-validator.ts
@@ -2,8 +2,9 @@ import { InvalidFieldLengthError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols'
 
 export class MinLengthValidation implements FieldValidation {
-  constructor (readonly field: string, private readonly fieldName, private readonly minLength: number) { }
+  constructor (readonly field: string, private readonly fieldName: string, private readonly minLength: number) { }
   validate (value: string): Error {
+    value = value.replace(/\s/g, '')
     return value.length >= this.minLength ? null : new InvalidFieldLengthError(this.fieldName, this.minLength)
   }
 }

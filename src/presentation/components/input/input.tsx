9258b5ea132a7a175f244cf67148c4b8a3560c70
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-29 21:05:42 -0300

    feat: ensure Login presents valid state if passqord is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -21,11 +21,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (

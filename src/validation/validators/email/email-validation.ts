b82909671e13a89137e093d55493111ddaecdbd7
Gervasio Artur <gervasioartur@outlook.com.br>
2022-10-30 23:04:15 -0300

    feat: ensure EmailValidator return falsy if email is empty

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -2,9 +2,9 @@ import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols'
 
 export class EmailValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor (readonly field: string) { }
   validate (value: string): Error {
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    return emailRegex.test(value) ? null : new InvalidFieldError('email')
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError('email')
   }
 }

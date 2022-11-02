f73b495d8805c3511b2fb30d268c236a20f214bf
Gervasio Artur <gervasioartur@outlook.com.br>
2022-11-02 15:26:37 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,23 +1,12 @@
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
+import { makeRemoteAuthentication } from '@/main/factories/useCases/authentication/remote-authentication-factory'
 import { Login } from '@/presentation/pages'
-import { ValidationBuilder } from '@/validation/validators/buider/validation-builder'
-import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
 import React from 'react'
+import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const axiosHttpClient = new AxiosHttpClient()
-  const url = 'http://fordevs.herokuapp.com/api/login'
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
-
-  const validationComposite = ValidationComposite.build([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
-
   return (
     <Login
-      authentication={remoteAuthentication}
-      validation={validationComposite} />
+      authentication={makeRemoteAuthentication()}
+      validation={makeLoginValidation()} />
   )
 }
---
+++ src/main/factories/useCases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteAuthentication } from '@/data/useCases/authentication/remote-authentication'
+import { Authentication } from '@/domain/useCases'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+
+export const makeRemoteAuthentication = (): Authentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient())
+}

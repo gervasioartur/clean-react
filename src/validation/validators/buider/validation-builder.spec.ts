4737c5e43f3df08ba04ece740086ce5ba140e331
Gervasio Artur <gervasioartur@outlook.com.br>
2022-11-02 14:13:13 -0300

    test: ensure ValidationBuilder returns EmailValidation

--- src/validation/validators/buider/validation-builder.spec.ts
+++ src/validation/validators/buider/validation-builder.spec.ts
@@ -1,9 +1,14 @@
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 import { ValidationBuilder } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   it('should return RequireFieldValidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  it('should return RequireFieldValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
